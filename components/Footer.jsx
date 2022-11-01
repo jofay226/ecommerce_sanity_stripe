faebedebfee963b65ce04232981ae3beb77f38cb
Javokhir Fayzullaev <javokhir226@Javokhirs-MacBook-Pro.local>
2022-11-01 11:19:45 +0500

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <p>2022 JooFay's online-market All rights reserved</p>
+      <p className='icons'> <AiFillInstagram/> <AiOutlineTwitter/> </p>
+    </div>
+  ) 
+}
+
+export default Footer
